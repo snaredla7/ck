5ec48f388b601a595a40b52f4bb315ddc3551bd0
snaredla7 <43969333+snaredla7@users.noreply.github.com>
2020-11-21 09:26:59 -0500

    Create aliash.sh

---
+++ aliash.sh
@@ -0,0 +1,13 @@
+alias a='alias'
+a k='kubectl'
+a ka='k apply'
+a kc='k create'
+a kd='k describe'
+a kdel='k delete'
+a ke='k edit'
+a kg='k get'
+a kr='k run'
+a kl='k logs'
+
+
+export do="--dry-run=client -o yaml"
